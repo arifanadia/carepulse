93deee1e1ee50e801a53326037931f6e5349fc13
arifanadia <contactarifanadia@gmail.com>
2024-08-31 21:54:51 +0600

    modal : admin passkey

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+
+  const isAdmin = searchParams.admin === 'true'
   return (
     <div className="flex h-screen max-h-screen">
-      {/* OTP verification || passkey modal */}
+      {
+        isAdmin && <PassKeyModal />
+      }
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container  max-w-[496px]">
           <Image
@@ -26,11 +31,11 @@ export default function Home() {
         </div>
       </section>
       <Image
-        src='/assets/images/onboarding-img.png' 
+        src='/assets/images/onboarding-img.png'
         height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]"/>
+        className="side-img max-w-[50%]" />
 
     </div>
   );
