06fcba4c263305bf8b300d6e0830a1bc1ce56899
arifanadia <contactarifanadia@gmail.com>
2024-08-31 16:04:15 +0600

    done : success page

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -4,17 +4,33 @@ import { ID } from "node-appwrite";
 import { DATABASE_ID, databases, APPOINTMENT_COLLECTION_ID } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-export const createAppointment = async (appointment : CreateAppointmentParams) => {
+export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
         const newAppointment = await databases.createDocument(
             DATABASE_ID!,
             APPOINTMENT_COLLECTION_ID!,
             ID.unique(),
-           appointment
+            appointment
         )
         console.log(newAppointment);
 
         return parseStringify(newAppointment)
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+export const getAppointment = async (appointmentId : string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId
+
+        )
+        return parseStringify(appointment)
+
     } catch (error) {
         console.log(error);
 
